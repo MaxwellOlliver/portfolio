992c297e7006ab180bbcfb9655ad1ae474f2b183
Maxwell Macedo <maxwellmacedo2015@gmail.com>
2025-07-15 00:48:13 -0300

    feat: add SVG logos

---
+++ src/components/GridSvg.tsx
@@ -0,0 +1,447 @@
+import * as React from "react";
+const GridSvg = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 1122 702"
+    fill="none"
+    preserveAspectRatio="none"
+    {...props}
+  >
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M28.31 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M55.638 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M82.948 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M110.275 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M137.586 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M164.897 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M192.224 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M219.534 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M246.846 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M274.172 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M301.483 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M328.81 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M356.121 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M383.432 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M410.759 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M438.069 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M465.396 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M492.707 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M520.018 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M547.345 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M574.655 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M601.982 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M629.293 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M656.604 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M683.931 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M711.241 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M738.568 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M765.879 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M793.19 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M820.517 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M847.828 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M875.139 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M902.466 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M929.775 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M957.103 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M984.414 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M1011.72 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1039.05 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.093}
+      d="M1066.36 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1093.69 1v700.129"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 675.192H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 649.27H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 623.333H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 597.412H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 571.475H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 545.538H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 519.616H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 493.68H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 467.758H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 441.821H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 415.884H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 389.962H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 364.025H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 338.104H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 312.167H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 286.229H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 260.308H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 234.371H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 208.449H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 182.513H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 156.591H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 130.654H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 104.717H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 78.795H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 52.858H1"
+    />
+    <path
+      stroke="#171717"
+      strokeDasharray="3.17 3.17"
+      strokeMiterlimit={10}
+      strokeWidth={1.584}
+      d="M1121 26.936H1"
+    />
+    <path
+      stroke="#171717"
+      strokeMiterlimit={10}
+      strokeWidth={1.109}
+      d="M1121 1H1v700.129h1120V1Z"
+    />
+  </svg>
+);
+export default GridSvg;
